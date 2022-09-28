10dfd5bc2a6f278865616da25a7641d33f6c759e
chanma <yunosuke.homma@icloud.com>
2022-09-29 04:40:52 +0900

    init

--- includes/philo.h
+++ includes/philo.h
@@ -7,7 +7,17 @@
 # include <unistd.h>
 # include <pthread.h>
 # include <sys/time.h>
+# include <limits.h>
 
 void	ft_putstr_fd(char *s, int fd);
+int		ft_atoi(const char *str);
+
+typedef struct s_info
+{
+	int	num_philo;
+	int	time_die;
+	int	time_sleep;
+	int	eat_times;
+}				t_info;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,26 @@
 #include "../includes/philo.h"
 
+void	init_info(t_info *info, int argc, char **argv)
+{
+	info->num_philo = ft_atoi(argv[1]);
+	info->time_die= ft_atoi(argv[2]);
+	info->time_sleep = ft_atoi(argv[3]);
+	if (argc == 5)
+		info->eat_times = ft_atoi(argv[4]);
+	else
+		info->eat_times = 0;
+	printf("%d\n", info->num_philo);
+	printf("%d\n", info->time_die);
+	printf("%d\n", info->time_sleep);
+	printf("%d\n", info->eat_times);
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 6 && argc != 7)
+	t_info	info;
+	if (argc != 4 && argc != 5)
 		ft_putstr_fd("usage: ./philo  [number_of_philosophers]\n\t\t[time_to_die time_to_eat]\n\t\t[time_to_sleep]\n      (optional)[number_of_times_each_philosopher_must_eat]\n", 2);
 	else
-		printf("%s\n", argv[0]);
+		init_info(&info, argc, argv);
 	return (0);
 }
