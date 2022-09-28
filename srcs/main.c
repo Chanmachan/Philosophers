bbdfdcb41b43d8568df102d32bbfb6c41c543c76
chanma <yunosuke.homma@icloud.com>
2022-09-29 03:31:46 +0900

    modify

--- srcs/main.c
+++ srcs/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-	if (argc != 6)
+	if (argc != 6 && argc != 7)
 		ft_putstr_fd("usage: ./philo  [number_of_philosophers]\n\t\t[time_to_die time_to_eat]\n\t\t[time_to_sleep]\n      (optional)[number_of_times_each_philosopher_must_eat]\n", 2);
 	else
 		printf("%s\n", argv[0]);
